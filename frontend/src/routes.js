745b93552a50621f9577e8faaee270edef3c7e54
herculesra <hercules.anselmo@ccc.ufcg.edu.br>
2020-09-25 14:50:17 -0300

    Clearning and initializate Frontend

---
+++ frontend/src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Home from './pages/Home';
+
+export default function Routes(){
+    return(
+        <BrowserRouter>
+        {/* o switch garante que apenas uma rota seja executada no momento */}
+            <Switch>
+                {/* exact evita que o '/' nao seja chamado quando acessado outra rota com '/' */}
+                <Route path="/" exact component={Home} />
+            </Switch>
+        </BrowserRouter>
+    );
+}
