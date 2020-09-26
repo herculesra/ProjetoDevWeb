c236d8ede85190fbca275c80184c3fa9ee82a69f
herculesra <hercules.anselmo@ccc.ufcg.edu.br>
2020-09-26 15:33:10 -0300

    Estados do componentes sendo alterados

--- frontend/src/components/MyCard/index.jsx
+++ frontend/src/components/MyCard/index.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 
 import './styles.css';
 
 const MyCard = (props) => {
+    const [nome, setNome] = useState('');
+    const [preco, setPreco] = useState(0);
+    const [categoria, setCategoria] = useState('');
+    const [qtdVendido, setQtdVendido] = useState(0);
+    const [favorito, setFavorito] = useState(false);
+    const [promocao, setPromocao] = useState(false);
+
+
+    useEffect(()=>{
+        if(props){
+            setNome(props.nome);
+            setPreco(props.preco);
+            setCategoria(props.categoria);
+            setQtdVendido(props.qtdVendido);
+            setFavorito(props.favorito);
+            setPromocao(props.promocao);
+        }
+    });
+
     return (
         <div className="my-card">
-            <h3>{props.nome}</h3>
-            <p>{props.preco}</p>
-            <p>{props.categoria}</p>
-            <p>Vendidos: {props.qtd_vendido}</p>
+            <h3>{nome}</h3>
+            <p>{preco}</p>
+            <p>{categoria}</p>
+            <p>Vendidos: {qtdVendido}</p>
             <button>Comprar</button>
             {/* adicionar icone Star */}
             <button>Favoritar</button>
